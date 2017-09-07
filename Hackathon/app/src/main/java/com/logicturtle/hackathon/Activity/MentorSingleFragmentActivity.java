d8e7cf3c18d51d741b87ac3f007a4892ca74bd45
Shrey <shreytripathi007@gmail.com>
2017-09-07 19:46:10 +0530

    SinglefragActvty and ViewPagerAdapter added

--- Hackathon/app/src/main/java/com/logicturtle/hackathon/Activity/MentorSingleFragmentActivity.java
+++ Hackathon/app/src/main/java/com/logicturtle/hackathon/Activity/MentorSingleFragmentActivity.java
@@ -9,8 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
 
-import hackathonayush.mentor.Model.MentorLightTextView;
-import hackathonayush.mentor.R;
+import com.logicturtle.hackathon.Model.MentorLightTextView;
+import com.logicturtle.hackathon.R;
 
 
 abstract public class MentorSingleFragmentActivity extends MentorActivity {
